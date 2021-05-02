5787d4fb9c2791b99b3e6996bae72589e3d75353
Suspir0n <evandrojunior1615@gmail.com>
2021-05-02 10:49:15 -0300

    update documentation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name='tcenter',
     packages=find_packages(include=['tcenter']),
-    version='0.1.4.2',
+    version='0.1.4.3',
     description='A library for center titles',
     author='Evandro Silva',
     license='MIT',
@@ -14,7 +14,7 @@ setup(
     tests_suite=['tests'],
     author_email='evandrojunior1615@gmail.com',
     url='https://github.com/Suspir0n/tcenter',
-    download_url='https://github.com/Suspir0n/tcenter/archive/refs/tags/0.1.4.2.tar.gz',
+    download_url='https://github.com/Suspir0n/tcenter/archive/refs/tags/0.1.4.3.tar.gz',
     python_requires='>=3.6',
     maintainer='Evandro Silva',
     maintainer_email='evandrojunior1615@gmail.com',
